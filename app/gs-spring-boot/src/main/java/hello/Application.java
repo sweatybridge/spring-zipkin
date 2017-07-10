7f15e8599fa4faa70b90b8bd11d9499fa8d23b6a
Han Qiao <qh812@imperial.ac.uk>
2017-07-11 04:41:20 +0800

    add search endpoint

--- app/gs-spring-boot/src/main/java/hello/Application.java
+++ app/gs-spring-boot/src/main/java/hello/Application.java
@@ -1,9 +1,15 @@
 package hello;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.sleuth.instrument.async.TraceableExecutorService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -12,13 +18,21 @@ import java.util.concurrent.Executors;
 @EnableAsync
 public class Application extends AsyncConfigurerSupport {
 
+  @Autowired
+  private BeanFactory beanFactory;
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
+  @Bean("executor")
   @Override
   public Executor getAsyncExecutor() {
-    return Executors.newFixedThreadPool(2);
+    return new TraceableExecutorService(beanFactory, Executors.newFixedThreadPool(2));
   }
 
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+    return restTemplateBuilder.build();
+  }
 }
--- app/gs-spring-boot/src/main/java/hello/HelloController.java
+++ app/gs-spring-boot/src/main/java/hello/HelloController.java
@@ -2,20 +2,27 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 // notice there is no tracing code in this class
 @RestController
 public class HelloController {
 
   private final EchoService echoService;
+  private final RestTemplate restTemplate;
+  private final Executor executor;
 
   @Autowired
-  public HelloController(EchoService echoService) {
+  public HelloController(EchoService echoService, RestTemplate restTemplate, Executor executor) {
     this.echoService = echoService;
+    this.restTemplate = restTemplate;
+    this.executor = executor;
   }
 
   @RequestMapping("/a")
@@ -28,5 +35,16 @@ public class HelloController {
     CompletableFuture.allOf(r1, r2, r3).join();
 
     return String.format("My school is %s %s %s.", r1.get(), r2.get(), r3.get());
+  }
+
+  @RequestMapping("/search")
+  public String abc(@RequestParam Integer echo) throws ExecutionException, InterruptedException {
+    CompletableFuture<String>[] rs = new CompletableFuture[echo];
+    for (int i = 0; i < echo; i++) {
+      String url = String.format("http://mockdb:8080/echo?name=%d", i);
+      rs[i] = CompletableFuture.supplyAsync(() -> restTemplate.getForObject(url, String.class), executor);
+    }
+    CompletableFuture.allOf(rs).join();
+    return "done";
   }
 }
